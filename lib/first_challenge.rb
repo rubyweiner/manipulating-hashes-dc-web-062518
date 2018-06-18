bc26d98442a6d8b9cd5da6e71756cdb530bc0803
Ruby Weiner <rubyweiner796@me.com>
2018-06-18 19:12:31 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == favorite_icecream_flavors
-        value.delete_if {|x| x == "strawberry"
+        value.delete_if {|x| x == "strawberry"}
       end
     end
     ["Freddy Mercury"][favorite_icecream_flavors]
